6a410e1c9f9c994ca024a9dcc113ef7e559271b1
junwoo <mkshie@naver.com>
2025-11-21 03:02:56 +0900

    feat : order 생성 api 추가

---
+++ src/main/java/org/monitoring/openmission/domain/order/controller/OrderController.java
@@ -0,0 +1,35 @@
+package org.monitoring.openmission.domain.order.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.monitoring.openmission.domain.order.dto.response.OrderResponse;
+import org.monitoring.openmission.domain.order.entity.Order;
+import org.monitoring.openmission.domain.order.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+@Tag(name = "주문 API", description = "생성 , 조회 , 삭제 , 수정 api 존재")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/{id}")
+    @Operation(summary = "주문 생성 API", description = "주문 ID 를 입력해주세요")
+    public ResponseEntity<OrderResponse> createOrder(
+            @PathVariable(name = "id") Long itemId,
+            @RequestParam(name = "quantity") @Positive Integer quantity
+    ) {
+        Order order = orderService.createOrder(itemId, quantity);
+        return ResponseEntity.ok(OrderResponse.of(order));
+    }
+}
---
+++ src/main/java/org/monitoring/openmission/domain/order/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package org.monitoring.openmission.domain.order.repository;
+
+import org.monitoring.openmission.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+}
