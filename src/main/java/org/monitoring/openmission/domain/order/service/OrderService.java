4ca6c03d94761e88e29470fd6e51d445d1dbbdae
junwoo <mkshie@naver.com>
2025-11-21 17:52:43 +0900

    refactor : code 재정렬

--- src/main/java/org/monitoring/openmission/domain/order/service/OrderService.java
+++ src/main/java/org/monitoring/openmission/domain/order/service/OrderService.java
@@ -20,9 +20,9 @@ public class OrderService {
     private final ItemService itemService;
 
     @Transactional
-    public Order createOrder(Long itemId , Integer quantity) {
+    public Order createOrder(Long itemId, Integer quantity) {
         Item item = itemRepository.getItemsById(itemId).orElseThrow(
-                () -> new EntityNotFoundException(itemId +"에 맞는 물건이 존재하지 않습니다."));
+                () -> new EntityNotFoundException(itemId + "에 맞는 물건이 존재하지 않습니다."));
 
         Order order = Order.builder()
                 .item(item)
