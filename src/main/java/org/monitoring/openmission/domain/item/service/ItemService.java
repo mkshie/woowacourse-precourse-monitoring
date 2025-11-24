8c153e132d806d99af0345eaebe746da220b48e9
junwoo <mkshie@naver.com>
2025-11-24 18:25:49 +0900

    refactor(service) : 코드 재정렬

--- src/main/java/org/monitoring/openmission/domain/item/service/ItemService.java
+++ src/main/java/org/monitoring/openmission/domain/item/service/ItemService.java
@@ -1,8 +1,12 @@
 package org.monitoring.openmission.domain.item.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.observation.annotation.Observed;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.monitoring.openmission.domain.item.dto.request.ItemCreateRequest;
 import org.monitoring.openmission.domain.item.dto.request.ItemUpdateRequest;
 import org.monitoring.openmission.domain.item.dto.response.ItemResponse;
@@ -15,34 +19,58 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final MeterRegistry meterRegistry; // 도메인 메트릭
+
 
     @Transactional
-    public Long createItem(ItemCreateRequest request){
-        Item item = Item.builder()
-                .name(request.name())
-                .price(request.price())
-                .stock(request.stock())
-                .build();
+    public Long createItem(ItemCreateRequest request) {
 
-        item = itemRepository.save(item);
+        log.info("item.create metric increment");
+        meterRegistry.counter("item.create.attempt").increment();
 
-        return item.getId();
+        long startTime = System.nanoTime();
+        boolean success = false;
+
+        try {
+            Item item = Item.builder()
+                    .name(request.name())
+                    .price(request.price())
+                    .stock(request.stock())
+                    .build();
+
+            item = itemRepository.save(item);
+
+            success = true;
+
+            return item.getId();
+        } catch (Exception e) {
+            log.info("item.create.failed", e);
+            meterRegistry.counter("item.create.failed", "exception", e.getClass().getSimpleName()
+            ).increment();
+
+            throw e;
+        } finally {
+            long duration = System.nanoTime() - startTime;
+            meterRegistry.timer("item.create.duration", "success", String.valueOf(success))
+                    .record(duration, TimeUnit.NANOSECONDS);
+        }
     }
 
     @Transactional
-    public ItemResponse updateItem(Long id , ItemUpdateRequest request) {
-        Item item = itemRepository.getItemsById(id).orElseThrow(()-> new EntityNotFoundException("Item not found"));
+    public ItemResponse updateItem(Long id, ItemUpdateRequest request) {
+        Item item = itemRepository.getItemsById(id).orElseThrow(() -> new EntityNotFoundException("Item not found"));
 
-        if(request.name() != null){
+        if (request.name() != null) {
             item.setName(request.name());
         }
-        if(request.price() != null){
+        if (request.price() != null) {
             item.setPrice(request.price());
         }
-        if(request.stock() != null){
+        if (request.stock() != null) {
             item.setStock(request.stock());
         }
         item = itemRepository.save(item);
@@ -51,13 +79,17 @@ public class ItemService {
     }
 
     @Transactional(readOnly = true)
-    public ItemResponse getItem(Long id){
-        Item item = itemRepository.getItemsById(id).orElseThrow(()-> new EntityNotFoundException("Item not found"));
+    @Observed(name = "Item.get", contextualName = "ItemService#getItem")
+    public ItemResponse getItem(Long id) {
+        log.info("getAllItems service start");
+        Item item = itemRepository.getItemsById(id).orElseThrow(() -> new EntityNotFoundException("Item not found"));
         return ItemResponse.of(item);
     }
 
+
     @Transactional(readOnly = true)
-    public List<ItemResponse> getAllItems(int page , int size) {
+    @Observed(name = "AllItem.get", contextualName = "ItemService#getAllItems")
+    public List<ItemResponse> getAllItems(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         List<Item> items = itemRepository.findAll(pageable).getContent();
         return items.stream().map(ItemResponse::of).toList();
